a615bdd14a27b7dad0b25bf294dfbf39a53a9ea8
Charles Fabricius --- CJ <cjsn22@gmail.com>
2019-08-19 04:43:56 +0000

    search box

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: '' 
+      searchField: ''
     };
   }
 
@@ -23,9 +23,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input 
-        type= 'search' 
-        placeholder=' Search Monsters' 
+        <input
+        type= 'search'
+        placeholder=' search monsters'
         onChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={this.state.monsters}/>

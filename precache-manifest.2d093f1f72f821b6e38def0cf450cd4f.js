c05683c85e4f785140528d5b0632148ebb02be6a
Charles Fabricius --- CJ <cjsn22@gmail.com>
2019-08-22 04:19:01 +0000

    Updates

---
+++ precache-manifest.2d093f1f72f821b6e38def0cf450cd4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d174adad2dd7ae708b7c4006a4562c52",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bbc9cf99e6c9aa23870a",
+    "url": "/monster-rolodex/static/css/main.bca3448a.chunk.css"
+  },
+  {
+    "revision": "054955b9be7763ffda6e",
+    "url": "/monster-rolodex/static/js/2.ca66acab.chunk.js"
+  },
+  {
+    "revision": "bbc9cf99e6c9aa23870a",
+    "url": "/monster-rolodex/static/js/main.cd9ba51c.chunk.js"
+  },
+  {
+    "revision": "7b4aa297f27b1c28bec4",
+    "url": "/monster-rolodex/static/js/runtime~main.345092ff.js"
+  }
+]);
